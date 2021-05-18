794ae41f3b55e313f8ee0e4c79af6649f56783f4
Michael DHaem <mikedhaem@me.com>
2021-05-18 06:25:10 -0700

    Initial Commit

---
+++ iScoreCards/iScoreCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iScoreCardsApp.swift
+//  iScoreCards
+//
+//  Created by Michael DHaem on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct iScoreCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
